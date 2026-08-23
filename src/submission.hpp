9b9bf048916087ada9fac6b47a903dcc830a344e
Humanoid-Human <121205339+Humanoid-Human@users.noreply.github.com>
2026-08-23 00:16:08 -0600

    basic solution

--- src/submission.hpp
+++ src/submission.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <cstddef>
+#include <vector>
+#include <algorithm>
 
 // Starter Grid for the 2D heat-diffusion problem.
 //
@@ -8,17 +10,54 @@
 // results; it never touches your internal storage. Keep this interface,
 // everything else is yours.
 class Grid {
-private:
-  std::size_t rows_;
-  std::size_t cols_;
+	private:
+	  std::size_t rows_;
+	  std::size_t cols_;
+	  std::vector<double> data;
 
-public:
-  Grid(std::size_t rows, std::size_t cols);
+	public:
+	  Grid(std::size_t rows, std::size_t cols);
 
-  double& operator()(std::size_t i, std::size_t j);
-  double  operator()(std::size_t i, std::size_t j) const;
-};  
+	  double& operator()(std::size_t i, std::size_t j);
+	  double  operator()(std::size_t i, std::size_t j) const;
 
-// Apply the five-point stencil over all interior points, copying the boundary
-// values unchanged from old_grid to new_grid. Implement your solution here.
-void apply_stencil(const Grid& old_grid, Grid& new_grid);
+	  const std::vector<double>& get_const_data() const { return data; }
+	  std::vector<double>& get_data() { return data; }
+	  std::size_t get_rows() const { return rows_; }
+	  std::size_t get_cols() const { return cols_; }
+
+	  friend void apply_stencil(const Grid& old_grid, Grid& new_grid);
+};
+
+Grid::Grid(std::size_t rows, std::size_t cols):
+	rows_{rows}, cols_{cols}, data{std::vector<double>(rows * cols, 0)} {}
+
+inline double& Grid::operator()(std::size_t i, std::size_t j) { return data[i * cols_ + j]; }
+inline double Grid::operator()(std::size_t i, std::size_t j) const { return data[i * cols_ + j]; }
+
+void apply_stencil(const Grid& old_grid, Grid& new_grid) {
+	const std::size_t rows = old_grid.rows_;
+	const std::size_t cols = old_grid.cols_;
+
+	// copy top and bottom row
+	std::copy_n(&old_grid.data[0], cols, &new_grid.data[0]);
+	std::copy_n(&old_grid.data[(rows - 1) * cols], cols,
+				&new_grid.data[(rows - 1) * cols]);
+
+	#pragma omp parallel for
+	for (int r = 1; r < rows - 1; r++) {
+		// copy ends
+		new_grid.data[r * cols] = old_grid.data[r * cols];
+		new_grid.data[(r+1) * cols - 1] = old_grid.data[(r+1) * cols - 1];
+
+		// actual stencil
+		// pragma omp simd doesn't seem to do anything here
+		for (int i = r * cols + 1; i < (r+1) * cols - 1; i++) {
+			const double u = old_grid.data[i + cols];
+			const double d = old_grid.data[i - cols];
+			const double l = old_grid.data[i - 1];
+			const double r = old_grid.data[i + 1];
+			new_grid.data[i] = 0.5 * old_grid.data[i] + 0.125 * (u + d + l + r);
+		}
+	}
+}
